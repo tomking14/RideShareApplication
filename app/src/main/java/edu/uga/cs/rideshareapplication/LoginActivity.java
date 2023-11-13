17ecb371a35fdddd7cd526c045823b6c5ee2310d
Mirpatel <82921562+Mirpatel@users.noreply.github.com>
2023-11-13 13:55:48 -0500

    updated sign in and layout activity

--- app/src/main/java/edu/uga/cs/rideshareapplication/LoginActivity.java
+++ app/src/main/java/edu/uga/cs/rideshareapplication/LoginActivity.java
@@ -1,4 +1,14 @@
 package edu.uga.cs.rideshareapplication;
 
-public class LoginActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginactivty);
+    }
+
 }
