416b70dc065b2736473f4125c7c1a8acd4dd4836
tomking14 <tomykastor@gmail.com>
2023-11-13 13:50:40 -0500

    activities

--- app/src/main/java/edu/uga/cs/rideshareapplication/SignUpActivity.java
+++ app/src/main/java/edu/uga/cs/rideshareapplication/SignUpActivity.java
@@ -1,4 +1,12 @@
 package edu.uga.cs.rideshareapplication;
 
-public class SignUpActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_activity);
+    }
 }
