8eda095bcdff78c40d83a57b6d06082d98b10214
tomking14 <tomykastor@gmail.com>
2023-11-14 11:24:38 -0500

    activities

--- app/src/main/java/edu/uga/cs/rideshareapplication/LoginActivity.java
+++ app/src/main/java/edu/uga/cs/rideshareapplication/LoginActivity.java
@@ -1,19 +1,65 @@
 package edu.uga.cs.rideshareapplication;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     private Button loginButton;
+    public static final String TAG = "SuperApp";
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginactivty);
 
         loginButton = findViewById(R.id.loginBtn);
-        loginButton.setOnClickListener(new ActivityStarterClass(HomePageActivity.class));
+//        loginButton.setOnClickListener(new ActivityStarterClass(HomePageActivity.class));
+        loginButton = findViewById(R.id.loginBtn);
+        TextInputEditText loginUsernameEditText = findViewById(R.id.login_username);
+        TextInputEditText loginPasswordEditText = findViewById(R.id.login_password);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Retrieve user inputs
+                String username = loginUsernameEditText.getText().toString();
+                String password = loginPasswordEditText.getText().toString();
+
+                // Authenticate with Firebase
+                mAuth.signInWithEmailAndPassword(username, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    Toast.makeText(LoginActivity.this, "Login successful.", Toast.LENGTH_SHORT).show();
+
+                                    // Use ActivityStarterClass to start HomePageActivity
+                                    ActivityStarterClass activityStarter = new ActivityStarterClass(HomePageActivity.class);
+                                    activityStarter.onClick(v); // You can pass the button itself as the view
+                                } else {
+                                    // Sign in fails
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+
 
     }
 
--- app/src/main/java/edu/uga/cs/rideshareapplication/MainActivity.java
+++ app/src/main/java/edu/uga/cs/rideshareapplication/MainActivity.java
@@ -1,25 +1,14 @@
 package edu.uga.cs.rideshareapplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button loginButton;
     private Button signupButton;
-    public static final String TAG = "SuperApp";
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,29 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         signupButton =  findViewById(R.id.signupBtn);
         signupButton.setOnClickListener(new ActivityStarterClass(SignUpActivity.class));
-        mAuth = FirebaseAuth.getInstance();
-        String email = "dawg@mail.com";
-        String password = "password";
 
-        mAuth.signInWithEmailAndPassword( email, password )
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d( TAG, "signInWithEmail:success" );
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText( MainActivity.this, "it worked!!.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                        else {
-                            // If sign in fails, display a message to the user.
-                            Log.d( TAG, "signInWithEmail:failure", task.getException() );
-                            Toast.makeText( MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
 
     }
 
