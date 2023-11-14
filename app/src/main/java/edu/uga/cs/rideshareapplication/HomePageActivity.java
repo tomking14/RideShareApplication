3b5366bea4c47a82c88c483c613ccb4b06baebac
tomking14 <tomykastor@gmail.com>
2023-11-14 12:21:42 -0500

    activities

--- app/src/main/java/edu/uga/cs/rideshareapplication/HomePageActivity.java
+++ app/src/main/java/edu/uga/cs/rideshareapplication/HomePageActivity.java
@@ -1,12 +1,39 @@
 package edu.uga.cs.rideshareapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class HomePageActivity extends AppCompatActivity {
+    private Button signOutButton;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage_activity);
+        signOutButton = findViewById(R.id.signoutBtn);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+
+            }
+        });
+    }
+    public void signOut() {
+        FirebaseAuth.getInstance().signOut();
+        Toast.makeText(this, "Signed out successfully.", Toast.LENGTH_SHORT).show();
+        goToMainActivity();
+        finish();
+        // Update UI to reflect that the user has been signed out
+    }
+    public void goToMainActivity() {
+        Intent intent = new Intent(HomePageActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
     }
 }
