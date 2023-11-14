171817a52a5026973604a4b8cf82287d564deebd
Mirpatel <82921562+Mirpatel@users.noreply.github.com>
2023-11-14 11:39:59 -0500

    working signup

--- app/src/main/java/edu/uga/cs/rideshareapplication/SignUpActivity.java
+++ app/src/main/java/edu/uga/cs/rideshareapplication/SignUpActivity.java
@@ -4,23 +4,59 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText emailField, passwordField;
+
     private Button confirmButton;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_activity);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emailField = findViewById(R.id.signup_username); // Assuming you have EditText for email
+        passwordField = findViewById(R.id.signup_password);
+
         confirmButton = findViewById(R.id.confirmBtn);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Registration Successful!!", Toast.LENGTH_SHORT).show();
-                goToMainActivity();
+                createAccount(emailField.getText().toString(), passwordField.getText().toString());
+
             }
         });
+    }
+
+    private void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(SignUpActivity.this, "Registration Successful!!", Toast.LENGTH_SHORT).show();
+                            goToMainActivity();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     public void goToMainActivity() {
